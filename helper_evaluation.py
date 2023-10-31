4f6e1ee480eaabf276f8bac29cd15f951242dbc2
Anna Putina <102035723+an-eve@users.noreply.github.com>
2023-10-31 08:08:29 +0300

    The first version of my U-net

---
+++ helper_evaluation.py
@@ -0,0 +1,60 @@
+import torch
+import segmentation_models_pytorch as smp
+
+def compute_accuracy_loss(model, data_loader, criterion, device="cuda"):
+    accuracy = 0
+    loss = 0
+    with torch.no_grad():
+
+        for i, (image, mask) in enumerate(data_loader):
+
+            image = image.float().to(device)
+            mask = mask.float().to(device)
+
+            mask_pred = model(image)
+
+            loss_batch = criterion(mask_pred,mask)
+            loss += loss_batch.item()
+
+            tp, fp, fn, tn = smp.metrics.get_stats(mask_pred, mask.int(),
+                                             threshold=0.5,
+                                             mode='binary')
+            accuracy_batch = smp.metrics.accuracy(tp, fp, fn, tn, reduction="micro").item()
+            accuracy += accuracy_batch
+    return accuracy / len(data_loader), loss / len(data_loader)
+    
+
+
+def compute_accuracy(model, data_loader, device="cuda"):
+    accuracy = 0
+    with torch.no_grad():
+
+        for i, (image, mask) in enumerate(data_loader):
+
+            image = image.float().to(device)
+            mask = mask.to(device)
+
+            mask_pred = model(image)
+            tp, fp, fn, tn = smp.metrics.get_stats(mask_pred, mask,
+                                             threshold=0.5,
+                                             mode='binary')
+            accuracy_batch = smp.metrics.accuracy(tp, fp, fn, tn, reduction="micro").item()
+
+            accuracy += accuracy_batch
+    return accuracy / len(data_loader)
+    
+    
+def compute_loss(model, data_loader, criterion, device="cuda"):
+
+    loss = 0
+    with torch.no_grad():
+        for i, (image, mask) in enumerate(data_loader):
+            image = image.float().to(device)
+            mask = mask.float().to(device)
+
+            mask_pred = model(image)
+            loss_batch = criterion(mask_pred,mask)
+
+            loss += loss_batch.item()
+
+    return loss / len(data_loader)
--- helper_train.py
+++ helper_train.py
@@ -1,28 +1,27 @@
 import time
 import torch
-from helper_evaluation import compute_accuracy
+from tqdm.auto import tqdm
+from helper_evaluation import compute_accuracy_loss
 
 
-def train_model(model, num_epochs, train_loader,
-                valid_loader, test_loader, optimizer,
-                device, logging_interval=50,
-                scheduler=None,
-                scheduler_on='valid_acc'):
+def train_model(model, num_epochs, train_loader, valid_loader, criterion,
+                optimizer, device, scheduler=None, scheduler_on='valid_loss'): # valid_loss or train_loss
 
-    start_time = time.time()
-    minibatch_loss_list, train_acc_list, valid_acc_list = [], [], []
-    
-    for epoch in range(num_epochs):
+    train_loss_list, valid_loss_list, valid_acc_list = [], [],  []
 
+    for epoch in tqdm(range(num_epochs)):
+
+        batch_loss_list = []
         model.train()
-        for batch_idx, (features, targets) in enumerate(train_loader):
+        for batch_idx, (image, mask) in enumerate(tqdm(train_loader)):
 
-            features = features.to(device)
-            targets = targets.to(device)
+
+            image = image.float().to(device)
+            mask = mask.float().to(device)
 
             # ## FORWARD AND BACK PROP
-            logits = model(features)
-            loss = torch.nn.functional.cross_entropy(logits, targets)
+            mask_pred = model(image)
+            loss = criterion(mask_pred,mask)
             optimizer.zero_grad()
 
             loss.backward()
@@ -31,39 +30,27 @@ def train_model(model, num_epochs, train_loader,
             optimizer.step()
 
             # ## LOGGING
-            minibatch_loss_list.append(loss.item())
-            if not batch_idx % logging_interval:
-                print(f'Epoch: {epoch+1:03d}/{num_epochs:03d} '
-                      f'| Batch {batch_idx:04d}/{len(train_loader):04d} '
-                      f'| Loss: {loss:.4f}')
+            batch_loss_list.append(loss.item())
 
         model.eval()
         with torch.no_grad():  # save memory during inference
-            train_acc = compute_accuracy(model, train_loader, device=device)
-            valid_acc = compute_accuracy(model, valid_loader, device=device)
-            print(f'Epoch: {epoch+1:03d}/{num_epochs:03d} '
-                  f'| Train: {train_acc :.2f}% '
-                  f'| Validation: {valid_acc :.2f}%')
-            train_acc_list.append(train_acc.item())
-            valid_acc_list.append(valid_acc.item())
+            train_loss =  sum(batch_loss_list) / len(batch_loss_list)
+            valid_acc, valid_loss = compute_accuracy_loss(model, valid_loader, criterion, device=device)
+            print(f'Epoch: {epoch+1:02d}/{num_epochs:02d} '
+                  f'| Train Loss: {train_loss :.4f} '
+                  f'| Validation Loss: {valid_loss :.4f} '
+                  f'| Validation Accuracy: {valid_acc*100 :.1f}%')
+            train_loss_list.append(train_loss)
+            valid_loss_list.append(valid_loss)
+            valid_acc_list.append(valid_acc)
 
-        elapsed = (time.time() - start_time)/60
-        print(f'Time elapsed: {elapsed:.2f} min')
-        
         if scheduler is not None:
 
-            if scheduler_on == 'valid_acc':
-                scheduler.step(valid_acc_list[-1])
-            elif scheduler_on == 'minibatch_loss':
-                scheduler.step(minibatch_loss_list[-1])
+            if scheduler_on == 'valid_loss':
+                scheduler.step(valid_loss_list[-1])
+            elif scheduler_on == 'train_loss':
+                scheduler.step(train_loss_list[-1])
             else:
                 raise ValueError(f'Invalid `scheduler_on` choice.')
-        
 
-    elapsed = (time.time() - start_time)/60
-    print(f'Total Training Time: {elapsed:.2f} min')
-
-    test_acc = compute_accuracy(model, test_loader, device=device)
-    print(f'Test accuracy {test_acc :.2f}%')
-
-    return minibatch_loss_list, train_acc_list, valid_acc_list
+    return train_loss_list, valid_loss_list, valid_acc_list
